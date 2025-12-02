3c7804a63890dc987baa1aae477d5856c4e8c5d3
artur-kamalov <camalov.arthur@gmail.com>
2025-12-02 21:56:45 +0500

    Jwt service

--- book-network/src/main/java/com/artur/book/security/JwtFilter.java
+++ book-network/src/main/java/com/artur/book/security/JwtFilter.java
@@ -39,6 +39,6 @@ public class JwtFilter extends OncePerRequestFilter {
     }
 
     jwt = authHeader.substring(7);
-    userEmail = jwtService.extractUsername(7);
+    userEmail = jwtService.extractUsername(jwt);
   }
 }
--- book-network/src/main/java/com/artur/book/security/JwtService.java
+++ book-network/src/main/java/com/artur/book/security/JwtService.java
@@ -1,21 +1,24 @@
 package com.artur.book.security;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
   private long jwtExpiration;
+  @Value("${application.security.jwt.secret-key}")
   private String secretKey; // TODO initialize secret key
 
   public String generateToken(UserDetails userDetails) {
@@ -43,13 +46,41 @@ public class JwtService {
       .compact();
   }
 
+  public boolean isTokenValid(String token, UserDetails userDetails) {
+    final String username = extractUsername(token);
+
+    return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+  }
+
+  private boolean isTokenExpired(String token) {
+    return extractExpiration(token).before(new Date());
+  }
+
+  private Date extractExpiration(String token) {
+    return extractClaims(token, Claims::getExpiration);
+  }
+
   private Key getSignInKey() {
     byte[] keyBytes = Decoders.BASE64.decode(secretKey);
 
-    return null;
+    return Keys.hmacShaKeyFor(keyBytes);
   }
 
-  public String extractUsername(Integer number) {
-    return "";
+  public String extractUsername(String token) {
+    return extractClaims(token, Claims::getSubject);
+  }
+
+  private <T> T extractClaims(String token, Function<Claims, T> claimsResolver) {
+    final Claims claims = extractAllClaims(token);
+    return claimsResolver.apply(claims);
+  }
+
+  private Claims extractAllClaims(String token) {
+    return Jwts
+      .parserBuilder()
+      .setSigningKey(getSignInKey())
+      .build()
+      .parseClaimsJws(token)
+      .getBody();
   }
 }
