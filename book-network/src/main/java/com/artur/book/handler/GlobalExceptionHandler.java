75c0ba06388945a9c202fedb13e2718041508a72
artur-kamalov <camalov.arthur@gmail.com>
2025-12-11 07:41:59 +0500

    OperationNotPermittedException

--- book-network/src/main/java/com/artur/book/handler/GlobalExceptionHandler.java
+++ book-network/src/main/java/com/artur/book/handler/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.artur.book.handler;
 
+import com.artur.book.exception.OperationNotPermittedException;
 import jakarta.mail.MessagingException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -91,6 +92,16 @@ public class GlobalExceptionHandler {
       .body(
         ExceptionResponse.builder()
           .businessErrorDescription("Internal errror, contact admin")
+          .error(exp.getMessage())
+          .build()
+      );
+  }
+
+  @ExceptionHandler(OperationNotPermittedException.class)
+  public ResponseEntity<ExceptionResponse> handleException(OperationNotPermittedException exp) {
+    return ResponseEntity.status(BAD_REQUEST)
+      .body(
+        ExceptionResponse.builder()
           .error(exp.getMessage())
           .build()
       );
